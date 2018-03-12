c1b8cf8163ad9a62bcfff03892ad7b356b3342c8
hao3304 <hao3304@qq.com>
2018-03-12 17:40:40 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div >
+    <div class="config-form " :class="{slider:slider}">
+      <h1>试卷生成器</h1>
+      <Form :label-width="80">
+        <FormItem label="计算类型">
+          <Select>
+            <Option value="A1" label="A1"></Option>
+          </Select>
+        </FormItem>
+        <FormItem label="题目数量">
+          <Input type="number" placeholder="请输入" />
+        </FormItem>
+      </Form>
+      <div>
+        <Button @click="onClick" type="success" style="width: 100%" size="large">生成</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        slider:false
+      }
+    },
+    methods:{
+      onClick() {
+        this.slider = true;
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+  .config-form{
+    h1{
+      text-align: center;
+      margin-bottom: 20px;
+      color: brown;
+    }
+    width: 500px;
+    position: absolute;
+    left: 50%;
+    top:20%;
+    margin-left: -250px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 1px 2px 2px rgba(0,0,0,.4);
+
+    transition: all .4s;
+
+    &.slider{
+      left: 0;
+      margin-left: 0;
+      width: 300px;
+      top:0;
+      bottom: 0;
+      border-radius: 0;
+    }
+  }
+</style>
